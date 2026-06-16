57d3c377d74fad2b0a57f2a8536391d7c7cef533
JUAN ANTONIO NUÑEZ ORTIZ <2024310311@uteq.edu.mx>
2026-06-16 13:00:31 -0600

    Add files via upload

--- database.py
+++ database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# Cadena de conexión para XAMPP local (Usuario: root, Password: "", Puerto: 3306)
-DATABASE_URL = "mysql+pymysql://root:@localhost:3306/impi_bot_db"
+# Cadena de conexión adaptada para la nube (SQLite en archivo local) en lugar de XAMPP
+DATABASE_URL = "sqlite:///./impi_bot.db"
 
-# Creamos el motor de la base de datos
-engine = create_engine(DATABASE_URL)
+# Creamos el motor de la base de datos (con configuración especial para SQLite)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 # Configuramos la "fábrica" de sesiones
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
